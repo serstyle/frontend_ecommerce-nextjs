9cbd44016c6f53379d8cf30f5c12dafe1d6b6a80
serstyle <desobry.f@gmail.com>
2020-09-15 15:45:46 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,33 +7,34 @@ function MyApp({ Component, pageProps }: AppProps) {
   const [cart, setCart] = React.useState(null);
 
   React.useEffect(() => {
-    console.log("useeffect")
-    document.addEventListener("snipcart.ready", () => {
-      console.log("snipcart ready")
-      let currentValue: any; // TODO: need to fix an interface
-      // (window as any).Snipcart.events.on(
-      //   "snipcart.initialized",
-      //   (snipcartState: any) => {
-      //     console.log("initialized")
-      // console.log(snipcartState);
-      // let previousValue = currentValue;
-      // currentValue = snipcartState;
+    console.log("useeffect");
+    // document.addEventListener("snipcart.ready", () => {
+    console.log("snipcart ready");
+    let currentValue: any; // TODO: need to fix an interface
+    // (window as any).Snipcart?.events.on(
+    //   "snipcart.initialized",
+    //   (snipcartState: any) => {
+    //     console.log("initialized")
+    // console.log(snipcartState);
+    // let previousValue = currentValue;
+    // currentValue = snipcartState;
 
-      // if (previousValue !== currentValue) {
-      //   const cart = snipcartState.cart;
-      //   setCart(cart);
-      //   console.log("cart", cart);
-      // }
-      const unsubscribe = (window as any).Snipcart.store.subscribe(() => {
-        console.log("sunbscrive")
-        let previousValue = currentValue;
-        currentValue = (window as any).Snipcart.store.getState();
-        console.log(previousValue, currentValue)
-        if (previousValue !== currentValue) {
-          const cart = (window as any).Snipcart.store.getState().cart;
-          setCart(cart);
-        }
-      });
+    // if (previousValue !== currentValue) {
+    //   const cart = snipcartState.cart;
+    //   setCart(cart);
+    //   console.log("cart", cart);
+    // }
+      (window as any).Snipcart?.store.subscribe(() => {
+      console.log("sunbscrsdvess");
+      let previousValue = currentValue;
+      currentValue = (window as any).Snipcart.store.getState();
+      console.log(previousValue, currentValue);
+      if (previousValue !== currentValue) {
+        const cart = (window as any).Snipcart.store.getState().cart;
+
+        setCart(cart);
+      }
+      // });
       // }
       // );
       // unsubscribe();
